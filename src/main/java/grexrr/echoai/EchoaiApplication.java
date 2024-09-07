8c917226b70a34540420bea323045b52ea397431
grexrr <wyatt_h.w@protonmail.com>
2024-09-07 17:12:19 +0100

    Initialize Spring Boot project files

---
+++ src/main/java/grexrr/echoai/EchoaiApplication.java
@@ -0,0 +1,13 @@
+package grexrr.echoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoaiApplication.class, args);
+	}
+
+}
