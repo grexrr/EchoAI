a447c865a2bef29bdca59bb99b901dd7425fc24f
grexrr <wyatt_h.w@protonmail.com>
2024-10-04 04:55:10 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,8 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-<<<<<<< HEAD
     // 替换为后端 API 地址
-    fetch('http://echoai-app-service:8080/public/hello')
-=======
     fetch('/api/public/hello')
->>>>>>> Cors
       .then((response) => response.text())
       .then((data) => setMessage(data))
       .catch((error) => console.error('Error fetching data:', error));
